7ade579988dd76d7cb999cef3b1c4032c1aedbe2
Kassidy <kassidyjo97@gmail.com>
2025-02-06 16:44:06 -0600

    finish header

--- angular-portfolio/src/app/shared/components/header/header.component.ts
+++ angular-portfolio/src/app/shared/components/header/header.component.ts
@@ -7,4 +7,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  logoString = 'TH';
+}
