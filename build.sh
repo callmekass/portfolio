7469b1127da253a256f7263bc918b28e428cb487
Kassidy <kassidyjo97@gmail.com>
2025-02-05 22:01:24 -0600

    add comment to build script

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# get path variables
 MVN_DIR=$(pwd)
 NG_DIR=$(pwd)"/"$(ls | grep angular-)
 
+# build angular project
 cd $NG_DIR
 rm -rf node_modules && npm install
 ng build --configuration production
 
+# build spring boot project
 cd $MVN_DIR
 mvn clean install
 
