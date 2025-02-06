d4fd8424d1b35dbd1fa91d98c49af78412187bc4
Kassidy <kassidyjo97@gmail.com>
2025-02-06 13:33:35 -0600

    setup basic page routing

--- angular-portfolio/src/app/app.routes.ts
+++ angular-portfolio/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { ProjectsComponent } from './page/projects/projects.component';
+import { BlogComponent } from './page/blog/blog.component';
+import { ContactComponent } from './page/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+];
