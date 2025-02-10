3bdad7e21357e672023093ed5ab10a897a9f75c8
Kassidy <kassidyjo97@gmail.com>
2025-02-09 21:27:31 -0600

    add social icons

--- angular-portfolio/src/app/pages/home/home.component.ts
+++ angular-portfolio/src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import {
+  faGithub,
+  faLinkedinIn,
+  faBluesky,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faGithub, faLinkedinIn, faBluesky);
+  }
 }
